342997e035fcfe86e422c5dea467f0d997d5695f
ClausCheshire <ilya131011@mail.ru>
2026-03-06 11:20:07 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,13 @@
+# -*- coding: utf-8 -*-
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-GIGACHAT_CLIENT_ID = os.getenv("GIGACHAT_CLIENT_ID")
-GIGACHAT_CLIENT_SECRET = os.getenv("GIGACHAT_CLIENT_SECRET")
+GIGACHAT_API_KEY = os.getenv("GIGACHAT_API_KEY")
 
-if not all([BOT_TOKEN, GIGACHAT_CLIENT_ID, GIGACHAT_CLIENT_SECRET]):
-    raise ValueError("Не все переменные окружения заполнены! Проверьте файл .env")
+if not BOT_TOKEN:
+    raise ValueError("BOT_TOKEN не найден в .env")
+if not GIGACHAT_API_KEY:
+    raise ValueError("GIGACHAT_API_KEY не найден в .env")
